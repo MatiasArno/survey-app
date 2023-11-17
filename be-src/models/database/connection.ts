2cb1f0076891bed56f162cd04881dcb9df51fb73
MatiasArno <amatiasarno@gmail.com>
2023-11-17 14:29:55 -0300

    DB Connection updated

--- be-src/models/database/connection.ts
+++ be-src/models/database/connection.ts
@@ -1,12 +1,13 @@
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
-const serviceAccount = JSON.parse(process.env.ADMIN_SDK_KEY as any);
+if (!process.env.ADMIN_SDK_KEY) {
+	throw new Error(
+		'Database JSON key is not loaded to the environment variables'
+	);
+}
 
-if (!process.env.ADMIN_SDK_KEY)
-throw new Error(
-	'Database JSON key is not loaded to the environment variables'
-);
+const serviceAccount = JSON.parse(process.env.ADMIN_SDK_KEY as any);
 
 initializeApp({
 	credential: cert(serviceAccount as any),
